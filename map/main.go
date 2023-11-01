5c22ddd016b496d19f8aa9c44d708c2878b241a9
Sean Cheng <ptp_sean@photons.com.tw>
2023-11-01 09:04:11 +0800

    update map

--- map/main.go
+++ map/main.go
@@ -6,43 +6,61 @@ func main() {
 	var m1 = make(map[int]int)
 	fmt.Printf("m1 : %v, %p\n", m1, &m1)
 
-	test1(&m1)
+	passMapAsPointer(&m1)
 	fmt.Printf("m1 : %v, %p\n\n", m1, &m1)
 
 	var m2 = make(map[int]int)
 	fmt.Printf("m2 : %v, %p\n", m2, &m2)
-	test2(m2)
+	passMapAsValue(m2)
 	fmt.Printf("m2 : %v, %p\n\n", m2, &m2)
 
-	var m3 = make(map[int]int)
+	var m3 map[int]int
 	fmt.Printf("m3 : %v, %p\n", m3, &m3)
-	test3(&m3)
+	passMapAsPointerAndRedefine(&m3)
 	fmt.Printf("m3 : %v, %p\n\n", m3, &m3)
 
-	var m4 = make(map[int]int)
+	var m4 map[int]int
 	fmt.Printf("m4 : %v, %p\n", m4, &m4)
-	test4(m4)
+	passMapAsValueAndRedefine(m4)
 	fmt.Printf("m4 : %v, %p", m4, &m4)
+
+	nilMapCheck()
 }
 
-func test1(m *map[int]int) {
-	fmt.Printf("m : %v, %p\n", m, m)
+func passMapAsPointer(m *map[int]int) {
 	(*m)[1] = 1
+	fmt.Printf("m : %v, %p\n", m, m)
 }
 
-func test2(m map[int]int) {
+func passMapAsValue(m map[int]int) {
 	m[1] = 1
 	fmt.Printf("m : %v, %p\n", m, &m)
 }
 
-func test3(m *map[int]int) {
+func passMapAsPointerAndRedefine(m *map[int]int) {
 	*m = make(map[int]int)
 	(*m)[1] = 1
 	fmt.Printf("m : %v, %p\n", m, m)
 }
 
-func test4(m map[int]int) {
+func passMapAsValueAndRedefine(m map[int]int) {
 	m = make(map[int]int)
 	m[1] = 1
 	fmt.Printf("m : %v, %p\n", m, &m)
+}
+
+func nilMapCheck() {
+	var m map[string]int
+
+	if m == nil {
+		fmt.Println("is nil map...")
+	} else {
+		fmt.Println("is not nil map...")
+	}
+
+	if len(m) == 0 {
+		fmt.Println("is nil map...")
+	} else {
+		fmt.Println("is not nil map...")
+	}
 }
